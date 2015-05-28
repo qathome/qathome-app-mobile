d6c4df2780d9b21d400171b4d89695607b097e3d
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2015-05-28 23:07:18 +0200

    Adding two (empty) activities. These will be the main (and likely only) application's activities (the MainActivity will host several fragment, if needed).

---
+++ src/com/qathome/qathome_app_mobile/LoginActivity.java
@@ -0,0 +1,7 @@
+package com.qathome.qathome_app_mobile;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+
+}
---
+++ src/com/qathome/qathome_app_mobile/MainActivity.java
@@ -0,0 +1,7 @@
+package com.qathome.qathome_app_mobile;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+}
